e24f491a6c227e4f2635c5de1c59e7526d75cee8
MaximGubanov <m_gubanov@bk.ru>
2022-09-16 12:34:19 +0300

    Добавлен в поиск Москва и СПб

--- main/run.py
+++ main/run.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     data = {
         'text': 'python AND ((drf OR restframework) OR django OR backend)',
         'date_from': datetime.now().strftime("%Y-%m-%dT00:00:00"),
-        'area': ['66'],  # поиск по Нижнему Новгороду
+        'area': ['66', '1', '2'],  # поиск по Нижнему Новгороду, Москве и СПб
     }
 
     session.run(data_to_send=data)
